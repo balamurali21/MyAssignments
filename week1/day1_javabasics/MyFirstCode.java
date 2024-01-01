058d0547951e156fd58bb8055cd99368cf134c9f
balamurali21 <126351840+balamurali21@users.noreply.github.com>
2024-01-02 03:18:36 +0530

    Delete week1/day1_javabasics directory

--- week1/day1_javabasics/MyFirstCode.java
+++
@@ -1,8 +0,0 @@
-package week1.day1;
-
-public class MyFirstCode {
-public static void main(String[] args) {
-	System.out.println("P.Balamuralikrishnan");
-	System.out.println("My First Code");
-}
-}
